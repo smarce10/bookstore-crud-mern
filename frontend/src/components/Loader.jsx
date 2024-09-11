e93ce7d63c9723b0201a115a7378ac35462291a7
marce <smarce18g@gmail.com>
2024-09-11 04:59:49 -0300

    agrego loader para cuando la informacion esté cargando

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+
+    return (
+        <>
+        <div className="flex w-full justify-center">
+            <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
+        </div>
+        </>
+    )
+}
+
+export default Loader
